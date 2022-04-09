ce2963e44df71f7f79c654b646dea31e2651afba
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-04-09 00:49:40 -0300

    Diferenças de projeto swift_kotlin para objc_java

---
+++ android/app/src/main/java/br/com/exemplo/flutter/exemplo_flutter_modelos/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.exemplo.flutter.exemplo_flutter_modelos;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/br/com/exemplo/flutter/exemplo_flutter_modelos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.exemplo.flutter.exemplo_flutter_modelos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
